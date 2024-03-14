ef84b802d0dcb47a7f09a351a4f3ba59460a3d5c
avi-sundaresan <92894634+avi-sundaresan@users.noreply.github.com>
2024-03-13 17:16:30 -0700

    file to load csv info into SQL database

---
+++ load-pokemon.sql
@@ -0,0 +1,21 @@
+-- This script loads in 3 CSV files to fill the pokedex, nature, and 
+-- type_weaknesses tables. 
+-- Use the following commands: 
+-- (if not created, enter CREATE DATABASE pokemondb first)
+-- USE DATABASE pokemondb; 
+-- source setup.sql; 
+-- source load-pokemon.sql; 
+
+LOAD DATA LOCAL INFILE 'data/pokedex.csv' INTO TABLE pokedex
+FIELDS TERMINATED BY ',' ENCLOSED BY '"' LINES TERMINATED 
+BY '\r\n' IGNORE 1 ROWS;
+
+LOAD DATA LOCAL INFILE 'data/nature.csv' INTO TABLE nature
+FIELDS TERMINATED BY ',' ENCLOSED BY '"' LINES TERMINATED 
+BY '\r\n' IGNORE 1 ROWS;
+
+LOAD DATA LOCAL INFILE 'data/type_weaknesses.csv' INTO TABLE type_weaknesses
+FIELDS TERMINATED BY ',' ENCLOSED BY '"' LINES TERMINATED 
+BY '\r\n' IGNORE 1 ROWS;
+
+
